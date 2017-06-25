117a7cdfc0f03aada9a0b603fd473ebade2795b9
Chaitanya Peramsetty <chaitanya.peramsetty@gmail.com>
2017-06-25 12:26:52 -0500

    complete assignment...

---
+++ src/main/java/org/launchcode/models/Menu.java
@@ -0,0 +1,58 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by cpera on 6/22/2017.
+ */
+@Entity
+public class Menu {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=3, max=15)
+    private String name;
+
+    @ManyToMany
+    private List<Cheese> cheeses;
+
+    public Menu(){
+    }
+
+    public Menu (String name)    {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addItem(Cheese item)
+    {
+        this.cheeses.add(item);
+    }
+
+    public List<Cheese> getCheeses()
+    {
+        return this.cheeses;
+    }
+
+
+}
